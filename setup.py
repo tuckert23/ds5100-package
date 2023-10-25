5b20687cce8cce25030c30c584bf748bc03c2215
tuckert23 <tuckert23@mail.wlu.edu>
2023-10-24 20:08:51 -0400

    edited setup

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from setuptools import setup
 
-setup(name="tools", version="0.01", description="Demo Package for Class", url = "https://github.com/tuckert23/ds5100-package",
-      author="Taylor Tucker", author_email="fake_email@example.com", license="MIT", packages=["tools"])
+setup(name="tools",
+      version="0.01",
+      description="Demo Package for Class", 
+      url = "https://github.com/tuckert23/ds5100-package",
+      author="Taylor Tucker", 
+      author_email="fake_email@example.com", 
+      license="MIT", 
+      packages=["tools"])
